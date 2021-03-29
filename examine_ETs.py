6d2a17ae3c71fabbdf5a09c5c9948a214b79046e
Lucas Ramos-Strankman <lucas+@tugboatlogic.com>
2021-03-29 10:49:09 -0600

    Made changes to use extract files from TBL platform

--- examine_ETs.py
+++ examine_ETs.py
@@ -8,7 +8,7 @@
 # Overview of how it works:
 # After reading the csv in, we create dictionaries for each framework
 # The control name is used as a key, and leads to a list of evidence task ids
-# Each control is then mapped to a list of ET ids that it requires
+# Each control is mapped to a list of ET ids that it requires
 # Using these dictionaries, we can make lists of "implemented" ETs
 # and see how many controls of another framework are satisfied by this list
 # Finally, we can plot this information using matplotlib
@@ -31,6 +31,59 @@ def parse_csv(filename):
         except csv.Error as e:
             sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
     return info
+
+# Takes the control_export as input and returns a dictionary of dictionaries
+# where each dictionary is a framework in tbl. These are then dictionaries with
+# their associated controls as keys mapping to empty lists
+# control_export must be in csv form
+def create_dict_of_frameworks(filename):
+    framework_dicts = {}
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        try:
+            next(reader, None) # This is to skip the header
+            for row in reader:
+                if row[6]: # col 6 has the frameworks, this checks it is not an empty cell
+                    frameworks_list = row[6].split('\n')
+                    for f in frameworks_list:
+                        if f in framework_dicts:
+                            framework = framework_dicts[f]
+                            framework[row[1]] = [] # col 1 has the control name
+                        else:
+                            framework = {}
+                            framework[row[1]] = []
+                            framework_dicts[f] =  framework # add framework to the framework dict
+        except csv.Error as e:
+            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+    return framework_dicts
+
+
+# takes a dictionary of dictionaries, each representing a framework in tbl
+# needs et_export in csv form
+# maps ET ids to controls in each dict using et_export file
+def fill_frameworks_with_ets(filename, framework_dicts):
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        try:
+            next(reader, None) # This is to skip the header
+            for row in reader:
+                framework_list = row[8].split('\n') # col 8 has the frameworks this et is used in
+                # print(framework_list)
+                for f in framework_list: # for each assocaited framework
+                    if f in framework_dicts:
+                        framework = framework_dicts[f]
+                        control_list = row[7].split('\n') # col 7 contains the associated controls to this et
+                        for control in control_list: # for each associated control
+                            if control in framework: # control is in this framework, find it and add the et to it
+                            #     print("CONTROL IN FRAMEWORK")
+                            # else:
+                            #     print("NOT IN FRAMEWORK")
+                                et_list = framework[control]
+                                et_list.append(row[0]) # col 0 has the ET id
+                    # else:
+                    #     print("err: encountered a framework that was not found")
+        except csv.Error as e:
+            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 
 
 # Adds the ET_id to a list for the given control, handles
@@ -97,9 +150,14 @@ def compare_framework_ETs(framework_list, fw2):
     implemented_ETs = find_implemented_ETs(framework_list)
     collected, partial, outstanding = control_implemented_status(implemented_ETs, fw2)
     output_to_file(collected, partial, outstanding) #outputs the results to a txt file
-    print("collected controls: {0}".format(len(collected)))
-    print("Partially collected controls: {0}".format(len(partial)))
-    print("Controls with unique ETs: {0}".format(len(outstanding)))
+    col_num = len(collected)
+    par_num = len(partial)
+    out_num = len(outstanding)
+    all_num = col_num + par_num + out_num
+    print("Collected controls: {0}".format(col_num))
+    print("Partially collected controls: {0}".format(par_num))
+    print("Controls with unique ETs: {0}".format(out_num))
+    print("Collected controls/rest = {}/{} = {:.2f}% ".format(col_num, all_num, float(col_num)/all_num*100 ))
 
 
 def dual_stacked_bar(fw1_dict, fw2_dict):
@@ -163,22 +221,27 @@ def single_stacked_bar(framework_list, fw1_dict):
         totals.append(value + outstanding_val)
 
     ind = np.arange(1)    # the x locations for the groups
-    width = 0.1       # the width of the bars: can also be len(x) sequence
+    width = .5       # the width of the bars: can also be len(x) sequence
 
-    p1 = plt.bar(ind, collected_vals, width, align='center')
-    p2 = plt.bar(ind, partial_vals, width,
+    p1 = plt.bar([1], collected_vals, width, align='center') # [1] is used so that the bar isn't so fat
+    p2 = plt.bar([1], partial_vals, width,
                  bottom=collected_vals, color='#f59542', align='center')
-    p3 = plt.bar(ind, outstanding_vals, width,
+    p3 = plt.bar([1], outstanding_vals, width,
               bottom=collected_and_partial, color='#f5424e', align='center')
 
+    implemented_framework_names = ""
+    for f in framework_list:
+        implemented_framework_names += " " + f['label']
+
+    plt.title('Control status after implementing {0}'.format(implemented_framework_names))
     plt.ylabel('Number of controls')
-    plt.title('Collected controls by framework')
-    plt.xticks(ind, ['{0}'.format(fw1_dict.get('label'))])
+    plt.xticks(np.arange(3), ["",fw1_dict['label'],""])
     plt.yticks(np.arange(0, max(totals)+50, 10))
     plt.legend((p1[0], p2[0], p3[0]), ["Collected", "Partially Collected", "Outstanding"],
         loc='upper center', ncol=3)
+    plt.subplots_adjust(left=0.05, right=0.95) # widen plot so legend fits
     plt.savefig("figure1.png")
-    plt.show()
+    # plt.show()
 
 
 def manage_header(header):
@@ -201,27 +264,48 @@ def contains_TSC(group_codes, check_code):
 
 
 def main():
-    file = "csv_ET.csv"
-    data = parse_csv(file)
-    framework_labels = manage_header(data.pop(0)) # Popped so the header row is removed
+    f = create_dict_of_frameworks('controls_export.csv')
+    fill_frameworks_with_ets('TugbotLogic-Evidence-Tasks.csv', f)
 
-    fw_dicts = []
-    for i in range(len(framework_labels)):
-        new_framework_dict = {'label':framework_labels[i]}
-        fw_dicts.append(new_framework_dict)
-        for row in data:
-            if contains_TSC(row[3], ""): # Used to filter by TSC Code, "" is no filter
-                ET_id = row[0]
-                control = row[4+i]
-                add_to_framework_dict(ET_id, control, new_framework_dict)
+    ### implemented
+    label1 = "NIST CSF"
+    ## investigating
+    label2 = "ISO 27001:2013"
 
-    print("~~~~~~~~~~~~")
-    print("{0} -> {1}".format(framework_labels[0], framework_labels[1]))
-    compare_framework_ETs([fw_dicts[0]], fw_dicts[1])
-    print("~~~~~~~~~~~~")
-    print("{0} -> {1}".format(framework_labels[1], framework_labels[0]))
-    compare_framework_ETs([fw_dicts[1]], fw_dicts[0])
-    dual_stacked_bar(fw_dicts[0], fw_dicts[1])
+    x = f[label1]
+    s = f[label2]
+
+    x['label'] = label1
+    s['label'] = label2
+
+    frameworks_list = []
+    frameworks_list.append(x)
+
+    compare_framework_ETs(frameworks_list, s)
+    single_stacked_bar(frameworks_list, s)
+
+
+    # file = "csv_ET.csv"
+    # data = parse_csv(file)
+    # framework_labels = manage_header(data.pop(0)) # Popped so the header row is removed
+    #
+    # fw_dicts = []
+    # for i in range(len(framework_labels)):
+    #     new_framework_dict = {'label':framework_labels[i]}
+    #     fw_dicts.append(new_framework_dict)
+    #     for row in data:
+    #         if contains_TSC(row[3], ""): # Used to filter by TSC Code, "" is no filter
+    #             ET_id = row[0]
+    #             control = row[4+i]
+    #             add_to_framework_dict(ET_id, control, new_framework_dict)
+    #
+    # print("~~~~~~~~~~~~")
+    # print("{0} -> {1}".format(framework_labels[0], framework_labels[1]))
+    # compare_framework_ETs([fw_dicts[0]], fw_dicts[1])
+    # print("~~~~~~~~~~~~")
+    # print("{0} -> {1}".format(framework_labels[1], framework_labels[0]))
+    # compare_framework_ETs([fw_dicts[1]], fw_dicts[0])
+    # dual_stacked_bar(fw_dicts[0], fw_dicts[1])
     # single_stacked_bar([fw_dicts[0]], fw_dicts[1]) # Examines ISO assuming SOC is implemented
 
 
